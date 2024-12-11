2898b23059d538a401355f31a497201cd780306c
amnipriyasowrirajan <amnipriya31@gmail.com>
2024-12-11 06:08:13 -0500

    December10 v2

---
+++ app/src/main/java/com/example/androiddecember10/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.androiddecember10;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Use the appropriate layout for SecondActivity
+        setContentView(R.layout.activity_second);
+    }
+}
